2fdf6ce2e84c01de82f5c6b85a45437109f58521
Eric0417 <wongeric1417@gmail.com>
2026-03-19 01:34:33 +0800

    pre-built frontend, fix build command

--- render-build.sh
+++ render-build.sh
@@ -1,15 +1,3 @@
-#!/bin/bash
-set -e
-
-echo "===== 安裝前端依賴並打包 ====="
-cd frontend
-npm install
-npm run build
-
-echo "===== 複製到後端 static ====="
-rm -rf ../backend/static
-cp -r dist ../backend/static
-
-echo "===== 安裝後端依賴 ====="
-cd ../backend
+#!/usr/bin/env bash
+cd backend
 pip install -r requirements.txt
